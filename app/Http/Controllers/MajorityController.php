9504da63ea318ba84bcb6d89c253776ccbfb6926
Bayu Saputra <93852223+bayusptrshop@users.noreply.github.com>
2024-09-17 15:36:54 +0700

    Dev sp3 sq2 (change jwt to auth user in majority) (#40)
    * Add: school migration table
    
    * Add: data seeder for school table
    
    * Add: School Model
    
    * Add: school endpoint api
    
    Add school endpoint for create, get list, update and delete school data
    
    * Fix: school model schemas for documentation
    
    * Fix: resetpassword endpoint
    
    Modified controller and function for api routes
    
    * Add SchoolControllers: createSchool, getAllSchools, getUpdateSchool, deleteSchool
    
    * Refactor: School models for documentation
    
    * Refactor: Update routes for School Endpoints
    
    * Refactor: School function for CRUD
    
    * Refactor: HTTP status code and add exception handling
    
    * Add delete and search functionality for school data
    
    * Commit message for changes in School.php
    
    * chore: rename table school
    
    * fix: school endpoint access with auth
    
    * Fix: add validator for delete & search, add SchoolResource, school schemas doc
    
    * fix: date format, exception update for same UUID, perPage option
    
    * fix: exception update based on same UUID
    
    * chores: changed date_format to only date
    
    * feat: add relationship 1-many school and user
    
    * Problem Created By
    
    * Add endpoints for creating and retrieving major data
    
    * Add endpoint for show detail majority
    
    * Update LoginController.php
    
    * Update 2024_09_02_015424_update_users_table.php
    
    * Update api.php
    
    * feat: add update and delete for majority endpoint
    
    * Change JWT to Auth user
    
    ---------
    
    Co-authored-by: astrocoding <zaenalalfian20@gmail.com>
    Co-authored-by: Zaenal Alfian <118641939+astrocoding@users.noreply.github.com>
    Co-authored-by: Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>

--- app/Http/Controllers/MajorityController.php
+++ app/Http/Controllers/MajorityController.php
@@ -6,21 +6,11 @@ use App\Models\Major;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Firebase\JWT\JWT;
-use Firebase\JWT\ExpiredException;
-use Firebase\JWT\Key;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class MajorityController extends Controller
 {
-    // GET USER UUID
-    public function user_uuid()
-    {
-        $token = Request()->bearerToken();
-        $jwt = Jwt::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
-        $token = $jwt->sub;
-        return $token;
-    }
     // GET MAJORITY
     public function index()
     {
@@ -48,13 +38,12 @@ class MajorityController extends Controller
         }
         try {
             $data = $validator->validated();
-            $user = User::where('uuid', $this->user_uuid())->first();
-            $data['created_by'] = $user->name;
+            $data['created_by'] = Auth::user()->name;
             Major::create($data);
             return response()->json([
                 'success' => true,
                 'message' => 'Major created successfully',
-            ], 200);
+            ], 201);
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
@@ -79,13 +68,54 @@ class MajorityController extends Controller
     // UPDATE MAJORITY
     public function update(Request $request, $id)
     {
+        $validator = Validator::make($request->all(), [
+            "major_code" => [
+                "required",
+                "max:255",
+                Rule::unique('majors')->ignore($id, 'uuid')
+            ],
+            "major_name" => "required|string|max:255",
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 400);
+        }
+        try {
+            $data = $validator->validated();
+            $user = User::where('uuid', $this->user_uuid())->first();
+            $data['updated_by'] = $user->name;
+            Major::where('uuid', $id)->update($data);
+            return response()->json([
+                'success' => true,
+                'message' => 'Major updated successfully',
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 
 
     // DELETE MAJORITY
     public function destroy($id)
     {
-
+        $major = Major::where('uuid', $id)->first();
+        if ($major) {
+            $major->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Major deleted successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Major not found',
+            ], 404);
+        }
     }
 }
