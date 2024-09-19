07489f40577083c7865b9be4f231ce218859b363
alfiannurrizky <alfiannrzky0@gmail.com>
2024-09-19 11:41:55 +0700

    change school_id column to nullable in users table

--- database/migrations/2024_09_18_092208_update_users_table_nullable_columns.php
+++ database/migrations/2024_09_18_092208_update_users_table_nullable_columns.php
@@ -11,6 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->uuid("school_id")->nullable()->change();
             $table->uuid('major_id')->nullable()->change();
             $table->uuid('class_id')->nullable()->change();
             $table->uuid('partner_id')->nullable()->change();
