25691a0c945ac87c2405e4cf50a3f2d3a467fa71
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-09-27 11:55:24 +0700

    Update 2024_09_18_092208_update_users_table_nullable_columns.php

--- database/migrations/2024_09_18_092208_update_users_table_nullable_columns.php
+++ database/migrations/2024_09_18_092208_update_users_table_nullable_columns.php
@@ -11,10 +11,10 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid("school_id")->nullable()->change();
-            $table->uuid('major_id')->nullable()->change();
-            $table->uuid('class_id')->nullable()->change();
-            $table->uuid('partner_id')->nullable()->change();
+            // $table->uuid("school_id")->nullable()->change();
+            // $table->uuid('major_id')->nullable()->change();
+            // $table->uuid('class_id')->nullable()->change();
+            // $table->uuid('partner_id')->nullable()->change();
         });
     }
 
