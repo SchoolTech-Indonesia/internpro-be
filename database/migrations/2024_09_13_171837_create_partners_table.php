925e22d608f7964dbbc517d128c8fb992f90c683
mhdreyhand <muhammad.reyhan320@gmail.com>
2024-10-04 10:36:56 +0700

    remove nullable from file_sk table

--- database/migrations/2024_09_13_171837_create_partners_table.php
+++ database/migrations/2024_09_13_171837_create_partners_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('name', 255)->unique();
             $table->text('address');
             $table->text('logo');
-            $table->text('file_sk')->nullable();
+            $table->text('file_sk');
             $table->string('number_sk', 255)->unique();
             $table->dateTime('end_date_sk');
             $table->uuid('school_id')->nullable();
