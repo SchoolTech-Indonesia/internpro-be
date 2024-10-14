4ecf92fe282a1f7c5797b360a9f04ff7ed3ddbe3
unknown <fauzansalim16@gmail.com>
2024-10-14 23:53:23 +0700

    feat: create, index, search activity

--- database/seeders/InternshipSeeder.php
+++ database/seeders/InternshipSeeder.php
@@ -36,7 +36,21 @@ class InternshipSeeder extends Seeder
             'description' => 'Internship for data science and machine learning students.',
             'start_date' => '2024-03-01',
             'end_date' => '2024-07-01',
-            'school_id' => $school->uuid, // Sesuaikan dengan data yang ada
+            'school_id' => $school->uuid,
+        ]);
+        DB::table('internships')->insert([
+            [
+                'uuid' => Str::uuid(),
+                'code' => Str::uuid(),
+                'name' => 'Data Science Internship',
+                'description' => 'Internship for data science and machine learning students.',
+                'start_date' => '2024-03-01',
+                'end_date' => '2024-07-01',
+                'school_id' => '1cd02e53-1233-47a5-94b4-768a8c5e2c4f', // Sesuaikan dengan data yang ada
+                'created_by' => '9d15e098-258d-4c8a-822c-331211f7b183', // Sesuaikan dengan user UUID yang valid
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
         ]);
     }
 }
