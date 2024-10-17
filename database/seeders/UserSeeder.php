ab3743e77091b0809a270b55ce7a7f655bb43198
alfiannurrizky <alfiannrzky0@gmail.com>
2024-10-17 16:59:34 +0700

    fix role seeder, from Koordinator to Coordinator

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -105,17 +105,17 @@ class UserSeeder extends Seeder
             'class_id' => $class->uuid,
         ]);
         $student->assignRole(['Student']);
-        
+
         $koordinator = User::firstOrCreate([
             'nip_nisn' => "123456",
             'name' => 'Test Koordinator',
             'email' => 'koordinator@dev-internpro.schooltech.biz.id',
             'phone_number' => "08551111232",
-            'password' => bcrypt('koordinator'),
+            'password' => bcrypt('Coordinator'),
             'school_id' => $school->uuid,
             'major_id' => $major->uuid,
         ]);
-        $koordinator->assignRole(['Koordinator']);
+        $koordinator->assignRole(['Coordinator']);
 
     }
 
