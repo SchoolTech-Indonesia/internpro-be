7aebc6a2b1fdfe17720d37de6e40ccad962b8d39
Muhammad Warisi <mw.a.seven.one@gmail.com>
2024-10-20 16:16:09 +0700

    add detail majority

--- app/Http/Controllers/OpportunityController.php
+++ app/Http/Controllers/OpportunityController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Opportunity;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
 
 class OpportunityController extends Controller
 {
@@ -48,7 +48,7 @@ class OpportunityController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
@@ -98,7 +98,29 @@ class OpportunityController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $opportunity = Opportunity::where('opportunity_id', $id)->first();
+
+            if (!$opportunity) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Data Opportunity Tidak Ditemukan',
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Detail Opportunity Jurusan',
+                'data' => $opportunity,
+            ], Response::HTTP_OK);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan saat mengambil data jurusan',
+                'error' => $e->getMessage(),
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
