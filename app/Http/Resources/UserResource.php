1c5a21c6c123545e66217d6fa2471c5085c65d8c
mhdreyhand <muhammad.reyhan320@gmail.com>
2024-10-22 16:06:07 +0700

    fix: repair deleted_by feature in CreatedBy traits

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,11 +20,11 @@ class UserResource extends JsonResource
             'email' => $this->email,
             'phone_number' => $this->phone_number,
             'roles' => $this->roles->pluck('name')->first(),
-            'major' => [
-                'uuid' => $this->major->uuid,
-                // 'major_code' => $this->major->major_code,
-                'major_name' => $this->major->major_name,
-            ],
+            // 'major' => [
+            //     'uuid' => $this->major->uuid,
+            //     // 'major_code' => $this->major->major_code,
+            //     'major_name' => $this->major->major_name,
+            // ],
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
         ];
